97b2dcaee677f49235ecff9b90daeaa894c6516b
dielawn <dielawnmerkill@gmail.com>
2024-02-25 09:38:35 -0700

    App test setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    
+    <h1>Our First Test</h1>
     </>           
   )
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
 
 import App from './App';
 
 describe('App', () => {
   it('renders headline', () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
+    render(<App />);
+    expect(screen.getByRole('heading')).toHaveTextContent(/our first test/i)
+   
     // check if App components renders headline
   });
 });
