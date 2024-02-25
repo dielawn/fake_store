c6ec77610b7f0eb4b5d498cb56aa6add3f96c3dc
dielawn <dielawnmerkill@gmail.com>
2024-02-25 11:39:07 -0700

    add to cart function adds then renders

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react'
 import './Cart.css'
 const todo = `drop down menu child of nav, cart icon, cart qty, adjust qty, remove item, checkout btn`
 
 export function Cart() {
+    const [cart, setCart] = useState([])
+    const [item, setItem] = useState({name: '', price: 0, qty: 0, id: null, })
+
+    function addToCart() {
+       if (item.name != '') {
+        setCart((prevItems) => [
+            ...prevItems, 
+            item
+        ])
+       }
+    }
+
+    function selectItem() {
+        setItem({ name: 'Test Item', price: 10, qty: 1 })
+    }
+
+    function adjustQty() {
+        
+    }
+
+
     
     return (
-        <p>{todo}</p>
+        
+        <div>
+            <button onClick={selectItem}>Select Item</button>
+            <button onClick={addToCart}>Add to Cart</button>
+            
+            {cart.map((item, index) => (
+                <div key={item.id}>{item.name} - {item.qty}</div>
+            ))}
+        </div>
     )
 }
--- src/Cart.test.jsx
+++ src/Cart.test.jsx
@@ -1,12 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 
 import {Cart} from './Cart'
 
 describe('Cart', () => {
-    it('renders a paragraph', () => {
+    it('adds an item to the cart when "Add to Cart" is clicked', async () => {
       render(<Cart />);
-     const para = screen.getByText(/[\S]/)
-     expect(para).toBeTruthy()
+
+      //simulate add item
+      fireEvent.click(screen.getByText('Select Item'))
+
+      //add uitem to the cart
+      fireEvent.click(screen.getByText('Add to Cart'))
+
+      //check if item was added to cart
+      const cartItem = await screen.findByText(/Test Item - 1/)
+      expect(cartItem).toBeInTheDocument()
+        
     });
   });
