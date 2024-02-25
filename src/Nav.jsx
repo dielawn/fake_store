d37e1f22cd20b44fed1df68d1f5e849d5ad942f2
dielawn <dielawnmerkill@gmail.com>
2024-02-25 10:35:50 -0700

    nav tests updated checks for links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,14 @@
+import { Link } from 'react-router-dom'
 import './Nav.css'
 const todo =  `links to home, shop, cart icon w/qty,`
 
 export function Nav() {
     return (
-        <p>{todo}</p>
+        <>
+            <p>{todo}</p>
+            <Link to='home'>Home</Link>
+            <Link to="shop">Store</Link>
+            <Link to='cart'>Cart</Link>
+        </>        
     )
 }
--- src/Nav.test.jsx
+++ src/Nav.test.jsx
@@ -1,12 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom'; 
 
 import {Nav} from './Nav'
 
 describe('Nav', () => {
-    it('renders a paragraph', () => {
-      render(<Nav />);
-     const para = screen.getByText(/[\S]/)
-     expect(para).toBeTruthy()
+    it('renders expected links and a paragraph', () => {
+      render(<MemoryRouter><Nav /></MemoryRouter>)
+
+      const todoParagraph = screen.getByText(/links to home, shop, cart icon w\/qty,/i);
+      expect(todoParagraph).toBeInTheDocument()
+
+      expect(screen.getByText('Home')).toBeInTheDocument();
+      expect(screen.getByText('Store')).toBeInTheDocument();
+      expect(screen.getByText('Cart')).toBeInTheDocument();  
+    
+
     });
   });
